ee5c5c9d8f6115fa5d1f6d20c50a4491544d3eee
ekpotu <wisdom.ekpotu@flutterwavego.com>
2023-12-06 15:52:14 +0100

    trigger

--- packages/cart/src/App.js
+++ packages/cart/src/App.js
@@ -3,7 +3,6 @@ import { ChakraProvider } from '@chakra-ui/react';
 import CartPage from './Page/CartPage';
 
 export default function App() {
-  // Wrap ChakraProvider at the root of your app
   return (
     <ChakraProvider>
       <CartPage />
--- packages/store/src/App.js
+++ packages/store/src/App.js
@@ -3,7 +3,6 @@ import { ChakraProvider } from '@chakra-ui/react';
 import StorePage from './Page/StorePage';
 
 export default function App() {
-  // Wrap ChakraProvider at the root of your app
   return (
     <ChakraProvider>
       <StorePage />
