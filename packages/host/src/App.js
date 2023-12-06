69523bc31ae66b84c0d8fcc401872b0b0ff05a73
ekpotu <wisdom.ekpotu@flutterwavego.com>
2023-12-06 15:29:45 +0100

    host trigger

--- packages/host/src/App.js
+++ packages/host/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Store_App from './Components/Store_App';
 import Cart_App from './Components/Cart_App';
-
 export default () => {
   return (
     <div>
